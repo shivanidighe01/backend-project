705fb145c777e09bc7485d84acb754f17f09410b
Shivani Dighe <shivanidighe2003@gmail.com>
2024-01-21 15:44:13 +0530

    connect to database

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async() =>
+{
+    try {
+        const conectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`host ${conectionInstance.connection.host}`)
+        
+    } catch (error) {
+        console.error("database connection error",error)
+       
+    }
+}
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-hello
+import dotenv from "dotenv";
+import connectDB from "./db/index.js"
+dotenv.config(
+    {
+        path:'./env'
+    }
+)
+
+connectDB()
